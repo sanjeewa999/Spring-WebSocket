10dd98b1d5b223875d44f9882430dfa9c7ad0a51
sanjeewa999 <sanjeewachaturanga0@gmail.com>
2023-04-25 22:10:06 +0530

    Initial commit

---
+++ Spring-WebSocket/src/main/java/com/ictec/SpringWebSocket/SpringWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.ictec.SpringWebSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSocketApplication.class, args);
+	}
+
+}
